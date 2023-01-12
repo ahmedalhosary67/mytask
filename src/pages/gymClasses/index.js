a71d97ac771618ac6fb174780f750bb302a71cf0
ahmed alhosary <ahmedalhosary67@gmail.com>
2023-01-12 19:56:07 +0200

    update solve issue

--- src/pages/gymClasses/index.js
+++ src/pages/gymClasses/index.js
@@ -23,57 +23,53 @@ const GymClasses = () => {
       <h1>GymClasses</h1>
       <Table striped bordered hover>
         <tbody>
-          {Object.entries(state)?.map((key) => (
-            <tr key={key}>
-              <td>{key[0]}</td>
-              <td>
-                <ol>
-                  {typeof key[1] == "string" ||
-                  typeof key[1] == "number" ||
-                  typeof key[1] == "boolean"
-                    ? key[1]
-                    : key[1]?.map((key2) => (
-                        <li>
-                          <ul>
-                            {Object.entries(key2)?.map((key3) => (
-                              <li>
-                                <span>{key3[0]}</span> : &nbsp;
-                                <span>
-                                  {typeof key3[1] == "string" ||
-                                  typeof key3[1] == "number" ||
-                                  typeof key3[1] == "boolean"
-                                    ? key3[1]
-                                    : key3[1]?.map((item) => (
-                                        <ul className="mb-3">
-                                          {Object.entries(item)?.map((key4) => (
-                                            <li>
-                                              <span>{key4[0]}</span> : &nbsp;
-                                              <span>{key4[1]}</span>
-                                            </li>
-                                          ))}
-                                        </ul>
-                                      ))}
-                                </span>
-                              </li>
-                            ))}
-                            <br />
-                          </ul>
-                        </li>
-                      ))}
-                </ol>
-              </td>
-            </tr>
-          ))}
+          {state != undefined &&
+            Object.entries(state)?.map((key) => (
+              <tr key={key}>
+                <td>{key[0]}</td>
+                <td>
+                  <ol>
+                    {typeof key[1] == "string" ||
+                    typeof key[1] == "number" ||
+                    typeof key[1] == "boolean"
+                      ? key[1]
+                      : key[1]?.map((key2) => (
+                          <li>
+                            <ul>
+                              {Object.entries(key2)?.map((key3) => (
+                                <li>
+                                  <span>{key3[0]}</span> : &nbsp;
+                                  <span>
+                                    {typeof key3[1] == "string" ||
+                                    typeof key3[1] == "number" ||
+                                    typeof key3[1] == "boolean"
+                                      ? key3[1]
+                                      : key3[1]?.map((item) => (
+                                          <ul className="mb-3">
+                                            {Object.entries(item)?.map(
+                                              (key4) => (
+                                                <li>
+                                                  <span>{key4[0]}</span> :
+                                                  &nbsp;
+                                                  <span>{key4[1]}</span>
+                                                </li>
+                                              )
+                                            )}
+                                          </ul>
+                                        ))}
+                                  </span>
+                                </li>
+                              ))}
+                              <br />
+                            </ul>
+                          </li>
+                        ))}
+                  </ol>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
-
-      {/* <ul>
-        {Object.entries(state)?.map((key) => (
-          <div key={key}>
-            <span>{key[0]}</span>
-          </div>
-        ))}
-      </ul> */}
     </div>
   );
 };
